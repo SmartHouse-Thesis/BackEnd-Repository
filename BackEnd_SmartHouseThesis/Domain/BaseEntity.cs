a676ca266c0853f71b1d5507199eac3ab486417b
TrungTruongTT <trungttse151131@fpt.edu.vn>
2024-01-03 23:58:20 +0700

    test_Push_CleanArchitecture

---
+++ BackEnd_SmartHouseThesis/Domain/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? ModificationDate { get; set; }
+        public Guid? ModificationBy { get; set; }
+        public bool? IsDelete { get; set; }
+        public DateTime? DeletionDate { get; set; }
+        public Guid? DeletedBy { get; set; }
+
+    }
+}
