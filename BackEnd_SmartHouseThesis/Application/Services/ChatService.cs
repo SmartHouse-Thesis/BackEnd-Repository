2b9d585bc8cee3ce98abd514da4d2ed0cbb70ab4
tuan-tu9173 <105278011+tuan-tu9173@users.noreply.github.com>
2024-01-27 22:28:59 +0700

    api for chat

---
+++ BackEnd_SmartHouseThesis/Application/Services/ChatService.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ChatService
+    {
+        private readonly ChatRepository _chatRepository;
+        public ChatService(ChatRepository chatRepository)
+        {
+            _chatRepository = chatRepository;
+        }
+        public async Task SaveChatLog(string chatMessage, string sender, string receiver) => await _chatRepository.SaveChatLog(chatMessage, sender, receiver);
+
+        public async Task<List<Chat>> GetChatLogs(Guid senderId, Guid receiverId) => await _chatRepository.GetChatLogs(senderId, receiverId);
+    }
+}
---
+++ BackEnd_SmartHouseThesis/BackEnd_SmartHouseThesis/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+using System;
+
+namespace BackEnd_SmartHouseThesis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ChatService _chatService;
+        public ChatController(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+        [HttpGet("GetChat/{senderId}/{receiverId}")]
+        public async Task<IActionResult> GetChat(Guid senderId, Guid receiverId)
+        {
+            var chatList = await _chatService.GetChatLogs(senderId, receiverId);
+            if (chatList == null)
+            {
+                return NotFound();
+            }
+            return Ok(chatList);
+        }
+    }
+}
