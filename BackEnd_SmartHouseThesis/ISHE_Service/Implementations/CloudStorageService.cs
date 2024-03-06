775c2a7606cb180dad623fbebf64e4de88dd6ba1
tuan-tu9173 <105278011+tuan-tu9173@users.noreply.github.com>
2024-03-06 16:57:16 +0700

    AccountService: Authenticate, GetAccounts, CreateAccount, GenerateJwtToken CloudStorageService: Upload, Delete, GetMediaLink CustomerService: Get, Create, UpdateCustomer, UploadAvatar OwnerService: Get, Create, UpdateOwner, UploadAvatar AccountController: GetAccounts AuthController: Authenticated CustomerController:  Get, Create, UpdateCustomer, UploadAvatar OwnerController: Get, Create, UpdateOwner, UploadAvatar Config Program.cs

---
+++ BackEnd_SmartHouseThesis/ISHE_Service/Implementations/CloudStorageService.cs
@@ -0,0 +1,80 @@
+﻿using Google.Cloud.Storage.V1;
+using Google;
+using ISHE_Utility.Helpers.CloudStorage;
+using ISHE_Utility.Settings;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using ISHE_Service.Interfaces;
+
+namespace ISHE_Service.Implementations
+{
+    public class CloudStorageService : ICloudStorageService
+    {
+        private static readonly StorageClient Storage;
+        private readonly AppSetting _settings;
+        static CloudStorageService()
+        {
+            Storage = CloudStorageHelper.GetStorage();
+        }
+
+        public CloudStorageService(IOptions<AppSetting> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public async Task<string> Upload(Guid id, string contentType, Stream stream)
+        {
+            try
+            {
+                await Storage.UploadObjectAsync(
+                    _settings.Bucket,
+                    $"{_settings.Folder}/{id}",
+                    contentType,
+                    stream,
+                    null,
+                    CancellationToken.None);
+                var baseURL = "https://firebasestorage.googleapis.com/v0/b";
+                var filePath = $"{_settings.Folder}%2F{id}";
+                var url = $"{baseURL}/{_settings.Bucket}/o/{filePath}?alt=media";
+                return url;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        // Delete an object, IsSuccess if deleted successfully or not found
+        public async Task<string> Delete(Guid id)
+        {
+            try
+            {
+                await Storage.DeleteObjectAsync(
+                    _settings.Bucket,
+                    $"{_settings.Folder}/{id}",
+                    null,
+                    CancellationToken.None
+                    );
+                return "Delete success";
+            }
+            catch (GoogleApiException ex)
+            {
+                return ex.HttpStatusCode.ToString();
+            }
+        }
+
+
+        // Object url
+        public string GetMediaLink(Guid id)
+        {
+            return CloudStorageHelper.GenerateV4UploadSignedUrl(
+                HttpUtility.UrlEncode(_settings.Bucket),
+                _settings.Folder + '/' + id);
+        }
+    }
+}
---
+++ BackEnd_SmartHouseThesis/ISHE_Service/Interfaces/ICustomerService.cs
@@ -0,0 +1,23 @@
+﻿using ISHE_Data.Models.Requests.Filters;
+using ISHE_Data.Models.Requests.Get;
+using ISHE_Data.Models.Requests.Post;
+using ISHE_Data.Models.Requests.Put;
+using ISHE_Data.Models.Views;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISHE_Service.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<ListViewModel<CustomerViewModel>> GetCustomers(CustomerFilterModel filter, PaginationRequestModel pagination);
+        Task<CustomerViewModel> GetCustomer(Guid id);
+        Task<CustomerViewModel> CreateCustomer(RegisterCustomerModel model);
+        Task<CustomerViewModel> UpdateCustomer(Guid id, UpdateCustomerModel model);
+        Task<CustomerViewModel> UploadAvatar(Guid id, IFormFile image);
+    }
+}
