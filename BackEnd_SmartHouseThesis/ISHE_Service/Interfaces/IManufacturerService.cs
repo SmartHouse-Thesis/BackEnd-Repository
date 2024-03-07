b2aa87cef8f79d2ad39778d836f25dd171e07df5
tuan-tu9173 <105278011+tuan-tu9173@users.noreply.github.com>
2024-03-08 01:02:50 +0700

    rebuild services: manufacturer, policy, promotion, smartdevice, staff, teller

---
+++ BackEnd_SmartHouseThesis/ISHE_Service/Interfaces/IManufacturerService.cs
@@ -0,0 +1,19 @@
+﻿using ISHE_Data.Models.Requests.Filters;
+using ISHE_Data.Models.Requests.Post;
+using ISHE_Data.Models.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISHE_Service.Interfaces
+{
+    public interface IManufacturerService
+    {
+        Task<List<ManufacturerViewModel>> GetManufacturers(ManufacturerFilterModel filter);
+        Task<ManufacturerViewModel> GetManufacturer(Guid id);
+        Task<ManufacturerViewModel> CreateManufacturer(CreateManufacturerModel model);
+        Task<ManufacturerViewModel> UpdateManufacturer(Guid id, CreateManufacturerModel model);
+    }
+}
---
+++ BackEnd_SmartHouseThesis/ISHE_Service/Interfaces/IPolicyService.cs
@@ -0,0 +1,18 @@
+﻿using ISHE_Data.Models.Requests.Post;
+using ISHE_Data.Models.Requests.Put;
+using ISHE_Data.Models.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISHE_Service.Interfaces
+{
+    public interface IPolicyService
+    {
+        Task<PolicyViewModel> GetPolicy(Guid id);
+        Task<PolicyViewModel> CreatePolicy(CreatePolicyModel model);
+        Task<PolicyViewModel> UpdatePolicy(Guid id, UpdatePolicyModel model);
+    }
+}
